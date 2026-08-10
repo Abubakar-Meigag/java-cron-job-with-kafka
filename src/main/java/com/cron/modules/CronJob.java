95574c2d2ed5aac4a08b78ac34e893d2f9dcf5d3
Abubakar-Meigag <113248042+Abubakar-Meigag@users.noreply.github.com>
2026-08-10 22:38:33 +0100

    create two classes CrontabParser and CronJob

--- src/main/java/com/cron/modules/CronJob.java
+++ src/main/java/com/cron/modules/CronJob.java
@@ -1,4 +1,21 @@
 package com.cron.modules;
 
 public class CronJob {
+
+    private int lineNumber;
+    private String cronExpression;
+
+    public CronJob(int lineNumber, String cronExpression) {
+        this.lineNumber = lineNumber;
+        this.cronExpression = cronExpression;
+    }
+
+
+    public int getLineNumber() {
+        return lineNumber;
+    }
+
+    public String getCronExpression() {
+        return cronExpression;
+    }
 }
--- src/main/java/com/cron/modules/CrontabParser.java
+++ src/main/java/com/cron/modules/CrontabParser.java
@@ -1,4 +1,23 @@
 package com.cron.modules;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CrontabParser {
+
+    public List<CronJob> parse(Path file) throws IOException {
+        List<String> lines = Files.readAllLines(file);
+        List<CronJob> jobs = new ArrayList<>();
+
+        for (int i = 0; i < lines.size(); i++) {
+            String line = lines.get(i);
+            if(!line.isEmpty()) {
+                jobs.add(new CronJob(i, line));
+            }
+        }
+        return jobs;
+    }
 }
